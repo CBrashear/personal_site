f901ee2a700d7444b88552830f6bd82a595931d9
CBrashear <chrisbrashear90@yahoo.com>
2022-04-09 19:36:10 -0500

    root_path displays url relative to the domain not _url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @resume_pdf_path = root_url + "pdf/Brashear_Christopher_Resume.pdf"
+    @resume_pdf_path = root_path + "pdf/Brashear_Christopher_Resume.pdf"
   end
 end
